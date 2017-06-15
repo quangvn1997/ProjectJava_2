c45e1207b2c2317b94119425b9481988f80bcd7e
quangvn1997 <quangdvd00285@fpt.edu.vn>
2017-06-15 16:37:07 +0700

    thao tac voi list table

--- src/console/restaurant/seeds/FoodSeed.java
+++ src/console/restaurant/seeds/FoodSeed.java
@@ -14,9 +14,7 @@ public class FoodSeed {
 	  // Database: online_shop
 	  Statement stt = DAO.getConnection().createStatement();
 	  // Thêm dữ liệu mẫu
-          stt.execute("INSERT INTO categories (name) VALUES ('Meat');");
-
-          stt.execute("INSERT INTO foods (name, category_id, description, unit_price, status) VALUES ('Cow Attack', '1','Cow Spicy','100.000', '1');");
+          stt.execute("INSERT INTO foods (name, category_id, description,img_url, unit_price, status) VALUES ('Cow Attack', '1','Cow Spicy','lol','100.000','1');");
 	  System.out.println("------ Chèn dữ liệu mẫu thành công! ------");
 	  // Đăng nhập với tài khoản: admin/admin hoặc guest/guest
      }
