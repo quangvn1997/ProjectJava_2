4c130644282463ba52550f911644f96a3799a746
quangvn1997 <quangdvd00285@fpt.edu.vn>
2017-06-28 23:07:02 +0700

    lam phan thong ke

--- src/console/restaurant/view/ManagerStatistic.java
+++ src/console/restaurant/view/ManagerStatistic.java
@@ -28,20 +28,26 @@ import javax.swing.table.DefaultTableModel;
  * @author Anh Tiến ơi.Có Trộm!
  */
 import com.toedter.calendar.JDateChooser;
+import console.restaurant.models.TablesModel;
+import java.awt.Font;
+import java.sql.Date;
 import java.text.DateFormat;
+
 public class ManagerStatistic extends JPanel {
 
     public static int page = 1;
     public static int limit = 14;
     public static int count = 0;
     public static int totalPage = 0;
-    public static String dateStart;
-    public static String dateEnd;
+//    public static String dateStart;
+//    public static String dateEnd;
+    private static float  fullPrice = 0;
 
     private static StatisticModel statisticModel = new StatisticModel();
 
     private JLabel lblStartD;
     private JLabel lblEndD;
+    private static JLabel lblFullPrice;
 
     private static JDateChooser startPicker;
     private static JDateChooser endPicker;
@@ -69,6 +75,10 @@ public class ManagerStatistic extends JPanel {
 
         this.lblEndD = new JLabel("Chọn Ngày Cuối Cùng:");
         this.lblEndD.setBounds(400, 20, 140, 34);
+        
+        this.lblFullPrice = new JLabel("Tổng Giá : " + fullPrice);
+        this.lblFullPrice.setBounds(700, 470, 200, 40);
+        this.lblFullPrice.setFont(new Font("Serif", Font.PLAIN,25));
         //Button.
         String[] listStrings = {"Theo hóa đơn", "Theo món"};
         this.cbList = new JComboBox(listStrings);
@@ -86,14 +96,21 @@ public class ManagerStatistic extends JPanel {
         this.btnNext.setBounds(520, 470, 50, 34);
         this.btnLast.setBounds(580, 470, 50, 34);
         //Date.
+        Calendar c = Calendar.getInstance();   // this takes current date
+         c.set(Calendar.DATE,Calendar.getInstance().getActualMinimum(Calendar.DATE));
         //Start.
         this.startPicker = new JDateChooser();
         this.startPicker.setBounds(160, 20, 200, 34);
-        this.startPicker.setDate(Calendar.getInstance().getTime());
+        this.startPicker.setDateFormatString("dd/MM/yyyy");
+        this.startPicker.setDate(c.getTime());
+        
         //End.
         this.endPicker = new JDateChooser();
         this.endPicker.setBounds(550, 20, 200, 34);
-        this.endPicker.setDate(Calendar.getInstance().getTime());
+        this.endPicker.setDateFormatString("dd/MM/yyyy");
+        c.set(Calendar.DATE, Calendar.getInstance().getActualMaximum(Calendar.DATE));
+        this.endPicker.setDate(c.getTime());
+        
         //Add Item.
         this.add(lblStartD);
         this.add(lblEndD);
@@ -105,17 +122,21 @@ public class ManagerStatistic extends JPanel {
         this.add(btnPage);
         this.add(btnNext);
         this.add(btnLast);
+        this.add(this.lblFullPrice);
 
         //Table.
-        String[] columnNames1 = {"ID", "Tổng giá", "Ngày tạo"};
+        String[] columnNames1 = {"ID", "Giá Trước Giảm","Giá Sau Giảm","Giảm Giá","Bàn Thanh Toán", "Ngày Tạo"};
         Object[][] data1 = {};
         this.modelStatistic = new DefaultTableModel(data1, columnNames1);
         this.table = new JTable(modelStatistic);
         //chinh with column
         this.table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-        this.table.getColumnModel().getColumn(0).setPreferredWidth(300);
-        this.table.getColumnModel().getColumn(1).setPreferredWidth(350);
-        this.table.getColumnModel().getColumn(2).setPreferredWidth(350);
+        this.table.getColumnModel().getColumn(0).setPreferredWidth(50);
+        this.table.getColumnModel().getColumn(1).setPreferredWidth(200);
+        this.table.getColumnModel().getColumn(2).setPreferredWidth(200);
+        this.table.getColumnModel().getColumn(3).setPreferredWidth(200);
+        this.table.getColumnModel().getColumn(4).setPreferredWidth(200);
+        this.table.getColumnModel().getColumn(5).setPreferredWidth(142);
         this.table.setRowHeight(24);
         this.table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
         //Hiển thị kích thước bảng
@@ -127,8 +148,8 @@ public class ManagerStatistic extends JPanel {
 
         this.setLayout(null);
         this.setVisible(true);
-        loadStatistic();
-        
+        loadStatisticOrder();
+
         this.btnNext.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -161,73 +182,30 @@ public class ManagerStatistic extends JPanel {
             }
         });
 
-        this.cbList.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if (cbList.getSelectedIndex() == 0) {
-                    //Table.
-                    String[] columnNames = {"ID", "Tổng giá", "Ngày tạo"};
-                    DefaultTableModel model = (DefaultTableModel) table.getModel();
-                    Object[][] data = {};
-                    model = new DefaultTableModel(data, columnNames);
-                    table.setModel(model);
-                    //chinh with column
-                    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                    table.getColumnModel().getColumn(0).setPreferredWidth(300);
-                    table.getColumnModel().getColumn(1).setPreferredWidth(350);
-                    table.getColumnModel().getColumn(2).setPreferredWidth(350);
-                    table.setRowHeight(24);
-                    model.fireTableDataChanged();
-                    loadStatisticOrder();
-                } else {
-                    //Table.
-                    String[] columnNames = {"ID", "Món Ăn", "Ngày tạo"};
-                    DefaultTableModel model = (DefaultTableModel) table.getModel();
-                    Object[][] data = {};
-                    model = new DefaultTableModel(data, columnNames);
-                    table.setRowHeight(24);
-                    table.setModel(model);
-                    //chinh with column
-                    table.setAutoResizeMode(JTable.AUTO_RESIZE_OFF);
-                    table.getColumnModel().getColumn(0).setPreferredWidth(300);
-                    table.getColumnModel().getColumn(1).setPreferredWidth(350);
-                    table.getColumnModel().getColumn(2).setPreferredWidth(350);
-                    model.fireTableDataChanged();
 
-                }
-
-            }
-        });
 
     }
 
     // Lấy dữ liệu hiển thị ra bảng.
-    public static void loadStatistic() {
-        DefaultTableModel model = (DefaultTableModel) table.getModel();
-        model.setRowCount(0);
-        ArrayList<Order> listOrder = statisticModel.getListSatistic(page, limit);
-        listOrder.forEach((order) -> {
-            model.addRow(new Object[]{String.valueOf(order.getId()), order.getTotalPrice(), order.getCreatedAt()});
-        });
-        count = statisticModel.countActive();
-        totalPage = count / limit + (count % limit > 0 ? 1 : 0);
-        btnPage.setText(String.valueOf(page));
-        handlePaginateButton();
-    }
     public static void loadStatisticOrder() {
         DefaultTableModel model = (DefaultTableModel) table.getModel();
         model.setRowCount(0);
-
-        SimpleDateFormat formatDate = new SimpleDateFormat("yyyy/MM/dd");
-        dateStart = formatDate.format(startPicker.getDate()); 
-        dateEnd = formatDate.format(endPicker.getDate());
-        ArrayList<Order> listOrder = statisticModel.getListOrder(page, limit, dateStart, dateEnd);
+        TablesModel tableModel = new TablesModel(); 
+        Date day1 = new Date(startPicker.getDate().getTime());
+        Date day2 = new Date(endPicker.getDate().getTime());
+        ArrayList<Order> listOrder = statisticModel.getListOrder(page, limit, day1, day2);
+        ArrayList<Order> listOrder1 = statisticModel.getListOrder(day1, day2);
+        for (Order order : listOrder1) {
+          fullPrice += order.getRealPrice();  
+        }
         listOrder.forEach((order) -> {
-            model.addRow(new Object[]{String.valueOf(order.getId()), order.getTotalPrice(), order.getCreatedAt()});
+            model.addRow(new Object[]{order.getId(), order.getRealPrice(),order.getTotalPrice(),order.getDiscount()+"%",tableModel.getById(order.getTableId()).getName() ,order.getCreatedAt()});
         });
         count = statisticModel.countActive();
         totalPage = count / limit + (count % limit > 0 ? 1 : 0);
         btnPage.setText(String.valueOf(page));
         handlePaginateButton();
+        ManagerStatistic.lblFullPrice.setText("tổng giá : "+ fullPrice);
     }
 
     // Xử lý việc hiển thị các nút phân trang.
